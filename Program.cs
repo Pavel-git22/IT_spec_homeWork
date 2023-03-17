54c4c00aacfc12acf72538a495e8a98d544d6669
Павел <p.dolmatov75@gmail.com>
2023-03-17 21:06:00 +0300

    добавление программы начало

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//  "Hello", "2", "world", ":-)"
+
+// "Russia", "Denmark", "Kazan", "123", "1234" 
+
+string[] Array1 = { "Hello", "2", "world", ":-)" };
